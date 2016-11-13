f84581d606e2d30bf2e1b2acf3fe15d9f543e368
Daniel <deudan1010@gmail.com>
2016-11-13 21:28:52 +0100

    part5 connecting to database

--- controllers/todocontroller.js
+++ controllers/todocontroller.js
@@ -1,4 +1,17 @@
 var bodyParser=require("body-parser");
+var mongoose=require("mongoose");
+//connect database
+mongoose.connect("mongodb://test:test@ds147537.mlab.com:47537/todo");
+//Create a schema, ie a blueprint
+var todoSchema= new mongoose.Schema({
+  item: String
+});
+
+var Todo = mongoose.model("Todo", todoSchema);
+var item1= Todo({item:"get flowers"}).save(function(err){
+  if(err) throw err;
+  console.log("item saved");
+});
 
 var data=[{item:"get milk"}, {item:"walk dog"}, {item:"coding"}];
 var urlencodedParser=bodyParser.urlencoded({extended:false});
@@ -16,6 +29,5 @@ module.exports=function(app){
       return todo.item.replace(/ /g, "-") !== req.params.item;
     });
     res.json(data);
-
   });
 };
